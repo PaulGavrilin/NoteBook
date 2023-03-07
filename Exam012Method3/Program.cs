f7e167b1f1a08fb09c27882074f872103a81840a
Павел <pavlikgavr2008@ya.ru>
2023-03-07 21:46:41 +0300

    update8

--- Exam012Method3/Program.cs
+++ Exam012Method3/Program.cs
@@ -1,23 +1,23 @@
 ﻿// вид 1
-void Method1()
+//void Method1()
 {
-    Console.WriteLine("Автор...");
+//    Console.WriteLine("Автор...");
 }
 //Method1();
 
 // вид 2
-void Method2(string msg)
+//void Method2(string msg)
 {
-    Console.WriteLine(msg);
+//    Console.WriteLine(msg);
 }
 //Method2("Message Text");
 
-void Method21(string msg, int count)
+//void Method21(string msg, int count)
 {
     int i = 0;
-    while (i < count)
+//    while (i < count)
     {
-        Console.WriteLine(msg);
+//        Console.WriteLine(msg);
         i = i + 1;
     }
 }
---
+++ Exam017Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿//double Factorial (int n)
+{
+    // 1! = 1;
+    // 0! = 1;
+//    if(n == 1) return 1;
+//    else return n * Factorial(n - 1);
+}
+//for (int i = 1; i < 40; i = i + 1)
+{
+//    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+
+// числа Фибоначчи
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+//int Fibonacci(int n)
+{
+//    if (n == 1 || n == 2) return 1;
+//    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+//for (int i = 1; i < 10; i = i + 1)
+{
+//    Console.WriteLine(Fibonacci(i));
+}
