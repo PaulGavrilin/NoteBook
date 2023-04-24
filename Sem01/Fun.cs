821271532e0283f613dd1d67be501476f69fab66
Павел <pavlikgavr2008@ya.ru>
2023-04-24 23:45:09 +0300

    task_02_02

--- Sem01/Fun.cs
+++ Sem01/Fun.cs
@@ -1,12 +1,12 @@
 //тестовый образец C# образца 2020 года
-using system;
-namespace Task1
-{
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            console.WriteLine("Hello, World");
-        }
-    }
-}
+//using system;
+//namespace Task1
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            console.WriteLine("Hello, World");
+//        }
+//    }
+//}
