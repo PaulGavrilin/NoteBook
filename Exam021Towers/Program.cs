e62414d91f7c131bf89337e9a08f5ed7753aacf5
Павел <pavlikgavr2008@ya.ru>
2023-04-24 21:10:33 +0300

    start new line

--- Exam021Towers/Program.cs
+++ Exam021Towers/Program.cs
@@ -1,4 +1,4 @@
-﻿void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+﻿void Towers(string with = "1", string on = "3", string some = "2", int count = 5)
 {
     if (count > 1) Towers(with, some, on, count - 1);
     Console.WriteLine($"{with} >> {on}");
