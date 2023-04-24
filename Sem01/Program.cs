ba1f7c24acc67fc0dca62ae49e94e56ce6e207cc
Павел <pavlikgavr2008@ya.ru>
2023-04-24 22:42:46 +0300

    square_task_upd_3

--- Sem01/Program.cs
+++ Sem01/Program.cs
@@ -4,3 +4,16 @@ Console.WriteLine(a);
 
 Console.Write("Square is ");
 Console.WriteLine(a*a);
+
+//эта программа позволяет считать квадрат случайного числа от 1 до 15;
+
+//int a = 1;
+//Console.WriteLine(a*a);
+
+//простая прога для подсчета квадрата
+
+//Console.Write("Enter a number: ");
+//int a = int.Parse(Console.ReadLine() ?? "0"); //Команда чтобы считать число заданное в строке(можно гасить вертикальной строкой)
+//int result = a*a;
+//Console.Write("Square of number: ");
+//Console.WriteLine(result);
