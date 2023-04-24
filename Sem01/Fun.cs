55e9f68f35abf016ba7cec5bb84fc2df9874b16f
Павел <pavlikgavr2008@ya.ru>
2023-04-24 22:50:57 +0300

    added fun

---
+++ Sem01/Fun.cs
@@ -0,0 +1,12 @@
+//тестовый образец C# образца 2020 года
+using system;
+namespace Task1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            console.WriteLine("Hello, World");
+        }
+    }
+}
