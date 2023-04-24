3d6a8a8bbbec484ccc0369fe03afb3b87e1edb9e
Павел <pavlikgavr2008@ya.ru>
2023-04-24 23:47:38 +0300

    task_02_03

--- Sem01/Task02.cs
+++ Sem01/Task02.cs
@@ -1,7 +1,7 @@
 Console.Write("enter 1st number: ");
-int a = int.Parse(Console.ReadLine() ?? "0");
+int a = int.Parse(Console.ReadLine()!);
 Console.Write("enter 2nd number: ");
-int b = int.Parse(Console.ReadLine() ?? "0");
+int b = int.Parse(Console.ReadLine()!);
 if (a>b)
 {
     Console.Write("Biggest: ");
