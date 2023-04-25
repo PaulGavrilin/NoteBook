985812a589e475e40113ce8b4a1f82b032f965f5
Павел <pavlikgavr2008@ya.ru>
2023-04-25 15:37:16 +0300

    day's task_02

--- Sem01HW/Program.cs
+++ Sem01HW/Program.cs
@@ -1,13 +1,13 @@
 ﻿//По заданному номеру дня недели вывести его название
 Console.Write("enter day's number: ");
 int a = int.Parse(Console.ReadLine()!);
-string A = monday;
-string B = tuesday;
-string C = wednesday;
-string D = thursday;
-string E = friday;
-string F = saturday;
-string G = sunday;
+string monday = (Console.ReadLine();
+string tuesday = (Console.ReadLine();
+string wednesday = (Console.ReadLine();
+string thursday = (Console.ReadLine();
+string friday = (Console.ReadLine();
+string saturday = (Console.ReadLine();
+string sunday = (Console.ReadLine();
 if (int a = 1)
 Console.Write("today is ");
 Console.WriteLine(string A);
